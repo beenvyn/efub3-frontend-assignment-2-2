ffdeb140958875a031064aa49266c3dad694ecb1
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2023-09-23 23:32:45 +0900

    📝 : 음료 데이터 추가

---
+++ drink-order-app/src/DrinkList.js
@@ -0,0 +1,76 @@
+import americano from "./assets/coffee/americano.png";
+import cafeLatte from "./assets/coffee/cafeLatte.png";
+import cafeMoca from "./assets/coffee/cafeMoca.png";
+
+import choco from "./assets/shake/choco.png";
+import mango from "./assets/shake/mango.png";
+import strawberry from "./assets/shake/strawberry.png";
+
+import black from "./assets/bubble-tea/black.png";
+import taro from "./assets/bubble-tea/taro.png";
+
+export const BestList = [
+  {
+    name: "아메리카노",
+    price: 3500,
+    img: americano,
+  },
+  {
+    name: "초코셰이크",
+    price: 5000,
+    img: choco,
+  },
+  {
+    name: "타로버블티",
+    price: 5500,
+    img: taro,
+  },
+];
+export const CoffeeList = [
+  {
+    name: "아메리카노",
+    price: 3500,
+    img: americano,
+  },
+  {
+    name: "카페라떼",
+    price: 4000,
+    img: cafeLatte,
+  },
+  {
+    name: "카페모카",
+    price: 4500,
+    img: cafeMoca,
+  },
+];
+
+export const ShakeList = [
+  {
+    name: "초코셰이크",
+    price: 5000,
+    img: choco,
+  },
+  {
+    name: "딸기셰이크",
+    price: 5000,
+    img: strawberry,
+  },
+  {
+    name: "망고셰이크",
+    price: 6000,
+    img: mango,
+  },
+];
+
+export const BubbleteaList = [
+  {
+    name: "블랙버블티",
+    price: 5500,
+    img: black,
+  },
+  {
+    name: "타로버블티",
+    price: 5500,
+    img: taro,
+  },
+];
