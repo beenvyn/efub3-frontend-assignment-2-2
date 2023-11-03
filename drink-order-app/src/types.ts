4b847178da09f794299fc60b7b43558b87daf404
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2023-11-03 13:21:38 +0900

    ✨ : 타입 정의 인터페이스

---
+++ drink-order-app/src/types.ts
@@ -0,0 +1,14 @@
+export interface CardItemProps {
+  id: number;
+  name: string;
+  price: number;
+  img: string;
+}
+
+export interface CartItemProps {
+  id: number;
+  name: string;
+  price: number;
+  img: string;
+  quantity: number;
+}
